ec560d70af795752ddbaf3682bdddd92af056024
onezg <635555803@qq.com>
2022-05-24 23:54:34 +0800

    add and update

--- pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/controller/HelloWorldController.java
+++ pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/controller/HelloWorldController.java
@@ -16,32 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    private static final Logger log= LoggerFactory.getLogger(HelloWorldController.class);
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
-    private static final String Prefix="helloWorld";
+    private static final String Prefix = "helloWorld";
 
     @Autowired
     private OrderRecordMapper orderRecordMapper;
 
     /**
      * 测试SpringBoot整合是否有问题-hello world
+     *
      * @return
      */
-    @RequestMapping(value = Prefix+"/rabbitmq",method = RequestMethod.GET)
-    public BaseResponse rabbitmq(){
-        BaseResponse response=new BaseResponse(StatusCode.Success);
-        String str="rabbitmq的第二阶段-spring boot整合rabbitmq";
+    @RequestMapping(value = Prefix + "/rabbitmq", method = RequestMethod.GET)
+    public BaseResponse rabbitmq() {
+        BaseResponse response = new BaseResponse(StatusCode.Success);
+        String str = "rabbitmq的第二阶段-spring boot整合rabbitmq";
         response.setData(str);
         return response;
     }
 
     /**
      * 整合mybatis访问数据列表
+     *
      * @return
      */
-    @RequestMapping(value = Prefix+"/data/list",method = RequestMethod.GET)
-    public BaseResponse dataList(){
-        BaseResponse response=new BaseResponse(StatusCode.Success);
+    @RequestMapping(value = Prefix + "/data/list", method = RequestMethod.GET)
+    public BaseResponse dataList() {
+        BaseResponse response = new BaseResponse(StatusCode.Success);
         response.setData(orderRecordMapper.selectAll());
         return response;
     }
