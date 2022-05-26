0e6ae963871f18b5dd3081175536aa960accb6c5
onezg <635555803@qq.com>
2022-05-26 09:24:43 +0800

    update

--- pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/config/RabbitmqConfig.java
+++ pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/config/RabbitmqConfig.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -26,7 +27,7 @@ import java.util.Map;
 @Configuration
 public class RabbitmqConfig {
 
-    private static final Logger log= LoggerFactory.getLogger(RabbitmqConfig.class);
+    private static final Logger log = LoggerFactory.getLogger(RabbitmqConfig.class);
 
     @Autowired
     private Environment env;
@@ -39,10 +40,11 @@ public class RabbitmqConfig {
 
     /**
      * 单一消费者
+     *
      * @return
      */
     @Bean(name = "singleListenerContainer")
-    public SimpleRabbitListenerContainerFactory listenerContainer(){
+    public SimpleRabbitListenerContainerFactory listenerContainer() {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
@@ -56,34 +58,35 @@ public class RabbitmqConfig {
 
     /**
      * 多个消费者
+     *
      * @return
      */
     @Bean(name = "multiListenerContainer")
-    public SimpleRabbitListenerContainerFactory multiListenerContainer(){
+    public SimpleRabbitListenerContainerFactory multiListenerContainer() {
 
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factoryConfigurer.configure(factory,connectionFactory);
+        factoryConfigurer.configure(factory, connectionFactory);
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
         factory.setAcknowledgeMode(AcknowledgeMode.NONE);
-        factory.setConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.concurrency",int.class));
-        factory.setMaxConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.max-concurrency",int.class));
-        factory.setPrefetchCount(env.getProperty("spring.rabbitmq.listener.prefetch",int.class));
+        factory.setConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.concurrency", int.class));
+        factory.setMaxConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.max-concurrency", int.class));
+        factory.setPrefetchCount(env.getProperty("spring.rabbitmq.listener.prefetch", int.class));
         return factory;
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(){
+    public RabbitTemplate rabbitTemplate() {
 //        connectionFactory.setPublisherConfirms(true);
-//        connectionFactory.setPublisherConfirmType(CachingConnectionFactory.ConfirmType.CORRELATED); // =上面那行代码
-//        connectionFactory.setPublisherReturns(true);
+        connectionFactory.setPublisherConfirmType(CachingConnectionFactory.ConfirmType.CORRELATED); // =上面那行代码
+        connectionFactory.setPublisherReturns(true);
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMandatory(true);
-//        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
-//            @Override
-//            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-//                log.info("消息发送成功:correlationData({}),ack({}),cause({})",correlationData,ack,cause);
-//            }
-//        });
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                log.info("消息发送成功:correlationData({}),ack({}),cause({})", correlationData, ack, cause);
+            }
+        });
 //        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
 //            @Override
 //            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
@@ -103,55 +106,52 @@ public class RabbitmqConfig {
     //TODO：基本消息模型构建（对应RabbitContoller)
 
     @Bean
-    public DirectExchange basicExchange(){
-        return new DirectExchange(env.getProperty("basic.info.mq.exchange.name"), true,false);
+    public DirectExchange basicExchange() {
+        return new DirectExchange(env.getProperty("basic.info.mq.exchange.name"), true, false);
     }
 
     @Bean(name = "basicQueue")
-    public Queue basicQueue(){
+    public Queue basicQueue() {
         return new Queue(env.getProperty("basic.info.mq.queue.name"), true);
     }
 
     @Bean
-    public Binding basicBinding(){
+    public Binding basicBinding() {
         return BindingBuilder.bind(basicQueue()).to(basicExchange()).with(env.getProperty("basic.info.mq.routing.key.name"));
     }
 
 
-
     //TODO：商品抢单消息模型创建
     @Bean
-    public DirectExchange robbingExchange(){
-        return new DirectExchange(env.getProperty("product.robbing.mq.exchange.name"), true,false);
+    public DirectExchange robbingExchange() {
+        return new DirectExchange(env.getProperty("product.robbing.mq.exchange.name"), true, false);
     }
 
     @Bean(name = "robbingQueue")
-    public Queue robbingQueue(){
+    public Queue robbingQueue() {
         return new Queue(env.getProperty("product.robbing.mq.queue.name"), true);
     }
 
     @Bean
-    public Binding robbingBinding(){
+    public Binding robbingBinding() {
         return BindingBuilder.bind(robbingQueue()).to(robbingExchange()).with(env.getProperty("product.robbing.mq.routing.key.name"));
     }
-
-
 
 
     //TODO：并发配置-消息确认机制-listener
 
     @Bean(name = "simpleQueue")
-    public Queue simpleQueue(){
-        return new Queue(env.getProperty("simple.mq.queue.name"),true);
+    public Queue simpleQueue() {
+        return new Queue(env.getProperty("simple.mq.queue.name"), true);
     }
 
     @Bean
-    public TopicExchange simpleExchange(){
-        return new TopicExchange(env.getProperty("simple.mq.exchange.name"),true,false);
+    public TopicExchange simpleExchange() {
+        return new TopicExchange(env.getProperty("simple.mq.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding simpleBinding(){
+    public Binding simpleBinding() {
         return BindingBuilder.bind(simpleQueue()).to(simpleExchange()).with(env.getProperty("simple.mq.routing.key.name"));
     }
 
@@ -160,15 +160,15 @@ public class RabbitmqConfig {
 
 
     @Bean(name = "simpleContainer")
-    public SimpleMessageListenerContainer simpleContainer(@Qualifier("simpleQueue") Queue simpleQueue){
-        SimpleMessageListenerContainer container=new SimpleMessageListenerContainer();
+    public SimpleMessageListenerContainer simpleContainer(@Qualifier("simpleQueue") Queue simpleQueue) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
 //        container.setMessageConverter(new Jackson2JsonMessageConverter());
 
         //TODO：并发配置
-        container.setConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.concurrency",Integer.class));
-        container.setMaxConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.max-concurrency",Integer.class));
-        container.setPrefetchCount(env.getProperty("spring.rabbitmq.listener.prefetch",Integer.class));
+        container.setConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.concurrency", Integer.class));
+        container.setMaxConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.max-concurrency", Integer.class));
+        container.setPrefetchCount(env.getProperty("spring.rabbitmq.listener.prefetch", Integer.class));
 
         //TODO：消息确认-确认机制种类
         container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
@@ -179,21 +179,20 @@ public class RabbitmqConfig {
     }
 
 
-
     //TODO：用户商城下单实战
 
     @Bean(name = "userOrderQueue")
-    public Queue userOrderQueue(){
-        return new Queue(env.getProperty("user.order.queue.name"),true);
+    public Queue userOrderQueue() {
+        return new Queue(env.getProperty("user.order.queue.name"), true);
     }
 
     @Bean
-    public TopicExchange userOrderExchange(){
-        return new TopicExchange(env.getProperty("user.order.exchange.name"),true,false);
+    public TopicExchange userOrderExchange() {
+        return new TopicExchange(env.getProperty("user.order.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding userOrderBinding(){
+    public Binding userOrderBinding() {
         return BindingBuilder.bind(userOrderQueue()).to(userOrderExchange()).with(env.getProperty("user.order.routing.key.name"));
     }
 
@@ -202,8 +201,8 @@ public class RabbitmqConfig {
     private UserOrderListener userOrderListener;
 
     @Bean
-    public SimpleMessageListenerContainer listenerContainerUserOrder(@Qualifier("userOrderQueue") Queue userOrderQueue){
-        SimpleMessageListenerContainer container=new SimpleMessageListenerContainer();
+    public SimpleMessageListenerContainer listenerContainerUserOrder(@Qualifier("userOrderQueue") Queue userOrderQueue) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
 
 //        container.setMessageConverter(new Jackson2JsonMessageConverter());
@@ -218,17 +217,17 @@ public class RabbitmqConfig {
     //TODO：系统日志消息模型
 
     @Bean(name = "logSystemQueue")
-    public Queue logSystemQueue(){
-        return new Queue(env.getProperty("log.system.queue.name"),true);
+    public Queue logSystemQueue() {
+        return new Queue(env.getProperty("log.system.queue.name"), true);
     }
 
     @Bean
-    public TopicExchange logSystemExchange(){
-        return new TopicExchange(env.getProperty("log.system.exchange.name"),true,false);
+    public TopicExchange logSystemExchange() {
+        return new TopicExchange(env.getProperty("log.system.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding logSystemBinding(){
+    public Binding logSystemBinding() {
         return BindingBuilder.bind(logSystemQueue()).to(logSystemExchange()).with(env.getProperty("log.system.routing.key.name"));
     }
 
@@ -236,17 +235,17 @@ public class RabbitmqConfig {
     //TODO：用户操作日志消息模型
 
     @Bean(name = "logUserQueue")
-    public Queue logUserQueue(){
-        return new Queue(env.getProperty("log.user.queue.name"),true);
+    public Queue logUserQueue() {
+        return new Queue(env.getProperty("log.user.queue.name"), true);
     }
 
     @Bean
-    public DirectExchange logUserExchange(){
-        return new DirectExchange(env.getProperty("log.user.exchange.name"),true,false);
+    public DirectExchange logUserExchange() {
+        return new DirectExchange(env.getProperty("log.user.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding logUserBinding(){
+    public Binding logUserBinding() {
         return BindingBuilder.bind(logUserQueue()).to(logUserExchange()).with(env.getProperty("log.user.routing.key.name"));
     }
 
@@ -254,18 +253,18 @@ public class RabbitmqConfig {
     //TODO：发送邮件消息模型
 
     @Bean
-    public Queue mailQueue(){
-        return new Queue(env.getProperty("mail.queue.name"),true);
+    public Queue mailQueue() {
+        return new Queue(env.getProperty("mail.queue.name"), true);
     }
 
     @Bean
-    public DirectExchange mailExchange(){
-        return new DirectExchange(env.getProperty("mail.exchange.name"),true,false);
+    public DirectExchange mailExchange() {
+        return new DirectExchange(env.getProperty("mail.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding mailBinding(){
-       return BindingBuilder.bind(mailQueue()).to(mailExchange()).with(env.getProperty("mail.routing.key.name"));
+    public Binding mailBinding() {
+        return BindingBuilder.bind(mailQueue()).to(mailExchange()).with(env.getProperty("mail.routing.key.name"));
     }
 
 
@@ -273,83 +272,81 @@ public class RabbitmqConfig {
 
     //创建死信队列
     @Bean
-    public Queue simpleDeadQueue(){
-        Map<String, Object> args=new HashMap();
+    public Queue simpleDeadQueue() {
+        Map<String, Object> args = new HashMap();
 
         args.put("x-dead-letter-exchange", env.getProperty("simple.dead.exchange.name"));
         args.put("x-dead-letter-routing-key", env.getProperty("simple.dead.routing.key.name"));
         args.put("x-message-ttl", 10000);
 
-        return new Queue(env.getProperty("simple.dead.queue.name"),true,false,false,args);
+        return new Queue(env.getProperty("simple.dead.queue.name"), true, false, false, args);
     }
 
     //绑定死信队列-面向生产端
     @Bean
-    public TopicExchange simpleDeadExchange(){
-        return new TopicExchange(env.getProperty("simple.produce.exchange.name"),true,false);
+    public TopicExchange simpleDeadExchange() {
+        return new TopicExchange(env.getProperty("simple.produce.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding simpleDeadBinding(){
+    public Binding simpleDeadBinding() {
         return BindingBuilder.bind(simpleDeadQueue()).to(simpleDeadExchange()).with(env.getProperty("simple.produce.routing.key.name"));
     }
 
 
     //创建并绑定实际监听消费队列
     @Bean
-    public Queue simpleDeadRealQueue(){
-       return new Queue(env.getProperty("simple.dead.real.queue.name"),true);
+    public Queue simpleDeadRealQueue() {
+        return new Queue(env.getProperty("simple.dead.real.queue.name"), true);
     }
 
     @Bean
-    public TopicExchange simpleDeadRealExchange(){
-        return new TopicExchange(env.getProperty("simple.dead.exchange.name"),true,false);
+    public TopicExchange simpleDeadRealExchange() {
+        return new TopicExchange(env.getProperty("simple.dead.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding simpleDeadRealBinding(){
+    public Binding simpleDeadRealBinding() {
         return BindingBuilder.bind(simpleDeadRealQueue()).to(simpleDeadRealExchange()).with(env.getProperty("simple.dead.routing.key.name"));
     }
-
-
 
 
     //TODO：用户下单支付超时死信队列模型
 
     @Bean
-    public Queue userOrderDeadQueue(){
-        Map<String, Object> args=new HashMap();
-        args.put("x-dead-letter-exchange",env.getProperty("user.order.dead.exchange.name"));
-        args.put("x-dead-letter-routing-key",env.getProperty("user.order.dead.routing.key.name"));
-        args.put("x-message-ttl",10000);
+    public Queue userOrderDeadQueue() {
+        Map<String, Object> args = new HashMap();
+        args.put("x-dead-letter-exchange", env.getProperty("user.order.dead.exchange.name"));
+        args.put("x-dead-letter-routing-key", env.getProperty("user.order.dead.routing.key.name"));
+        args.put("x-message-ttl", 10000);
 
-        return new Queue(env.getProperty("user.order.dead.queue.name"),true,false,false,args);
+        return new Queue(env.getProperty("user.order.dead.queue.name"), true, false, false, args);
     }
 
     @Bean
-    public TopicExchange userOrderDeadExchange(){
-        return new TopicExchange(env.getProperty("user.order.dead.produce.exchange.name"),true,false);
+    public TopicExchange userOrderDeadExchange() {
+        return new TopicExchange(env.getProperty("user.order.dead.produce.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding userOrderDeadBinding(){
+    public Binding userOrderDeadBinding() {
         return BindingBuilder.bind(userOrderDeadQueue()).to(userOrderDeadExchange()).with(env.getProperty("user.order.dead.produce.routing.key.name"));
     }
 
 
     @Bean
-    public Queue userOrderDeadRealQueue(){
-        return new Queue(env.getProperty("user.order.dead.real.queue.name"),true);
+    public Queue userOrderDeadRealQueue() {
+        return new Queue(env.getProperty("user.order.dead.real.queue.name"), true);
     }
 
 
     @Bean
-    public TopicExchange userOrderDeadRealExchange(){
+    public TopicExchange userOrderDeadRealExchange() {
         return new TopicExchange(env.getProperty("user.order.dead.exchange.name"));
     }
 
     @Bean
-    public Binding userOrderDeadRealBinding(){
+    public Binding userOrderDeadRealBinding() {
         return BindingBuilder.bind(userOrderDeadRealQueue()).to(userOrderDeadRealExchange()).with(env.getProperty("user.order.dead.routing.key.name"));
     }
 
@@ -357,38 +354,38 @@ public class RabbitmqConfig {
     //TODO：死信队列动态设置TTL消息模型
 
     @Bean
-    public Queue dynamicDeadQueue(){
-        Map<String, Object> args=new HashMap();
-        args.put("x-dead-letter-exchange",env.getProperty("dynamic.dead.exchange.name"));
-        args.put("x-dead-letter-routing-key",env.getProperty("dynamic.dead.routing.key.name"));
+    public Queue dynamicDeadQueue() {
+        Map<String, Object> args = new HashMap();
+        args.put("x-dead-letter-exchange", env.getProperty("dynamic.dead.exchange.name"));
+        args.put("x-dead-letter-routing-key", env.getProperty("dynamic.dead.routing.key.name"));
 
-        return new Queue(env.getProperty("dynamic.dead.queue.name"),true,false,false,args);
+        return new Queue(env.getProperty("dynamic.dead.queue.name"), true, false, false, args);
     }
 
     @Bean
-    public TopicExchange dynamicDeadExchange(){
-        return new TopicExchange(env.getProperty("dynamic.dead.produce.exchange.name"),true,false);
+    public TopicExchange dynamicDeadExchange() {
+        return new TopicExchange(env.getProperty("dynamic.dead.produce.exchange.name"), true, false);
     }
 
     @Bean
-    public Binding dynamicDeadBinding(){
+    public Binding dynamicDeadBinding() {
         return BindingBuilder.bind(dynamicDeadQueue()).to(dynamicDeadExchange()).with(env.getProperty("dynamic.dead.produce.routing.key.name"));
     }
 
 
     @Bean
-    public Queue dynamicDeadRealQueue(){
-        return new Queue(env.getProperty("dynamic.dead.real.queue.name"),true);
+    public Queue dynamicDeadRealQueue() {
+        return new Queue(env.getProperty("dynamic.dead.real.queue.name"), true);
     }
 
 
     @Bean
-    public TopicExchange dynamicDeadRealExchange(){
+    public TopicExchange dynamicDeadRealExchange() {
         return new TopicExchange(env.getProperty("dynamic.dead.exchange.name"));
     }
 
     @Bean
-    public Binding dynamicDeadRealBinding(){
+    public Binding dynamicDeadRealBinding() {
         return BindingBuilder.bind(dynamicDeadRealQueue()).to(dynamicDeadRealExchange()).with(env.getProperty("dynamic.dead.routing.key.name"));
     }
 
--- pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/rabbitmq/CommonListener.java
+++ pangu-mq-rabbit-j2ee/src/main/java/com/debug/steadyjack/rabbitmq/CommonListener.java
@@ -1,10 +1,12 @@
 package com.debug.steadyjack.rabbitmq;
 
+import com.debug.steadyjack.dto.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,34 +26,34 @@ public class CommonListener {
      *
      * @param message
      */
-    @RabbitListener(queues = "${basic.info.mq.queue.name}", containerFactory = "singleListenerContainer")
-//    public void consumeMessage(@Payload byte[] message){ //原来的写法
-    public void consumeMessage(String message) {
-        try {
-            //TODO：接收String
-           /* String result=new String(message,"UTF-8");
-            log.info("接收到消息： {} ", result);*/  //原来的
-
-            log.info("接收到消息： {} ", message);
-        } catch (Exception e) {
-            log.error("监听消费消息 发生异常： ", e.fillInStackTrace());
-        }
-
-    }
+//    @RabbitListener(queues = "${basic.info.mq.queue.name}", containerFactory = "singleListenerContainer")
+////    public void consumeMessage(@Payload byte[] message){ //原来的写法
+//    public void consumeMessage(String message) {
+//        try {
+//            //TODO：接收String
+//           /* String result=new String(message,"UTF-8");
+//            log.info("接收到消息： {} ", result);*/  //原来的
+//
+//            log.info("接收到消息： {} ", message);
+//        } catch (Exception e) {
+//            log.error("监听消费消息 发生异常： ", e.fillInStackTrace());
+//        }
+//
+//    }
 
 
     /**
      * 监听消费消息-实体信息
      * 对应RabbitController的/object/message/send
      */
-//    @RabbitListener(queues = "${basic.info.mq.queue.name}", containerFactory = "singleListenerContainer")
-//    public void consumeMessage(@Payload User user) {
-//        try {
-//            log.info("接收到实体消息： {} ", user);
-//        } catch (Exception e) {
-//            log.error("监听消费消息 发生异常： ", e.fillInStackTrace());
-//        }
-//    }
+    @RabbitListener(queues = "${basic.info.mq.queue.name}", containerFactory = "singleListenerContainer")
+    public void consumeMessage(@Payload User user) {
+        try {
+            log.info("接收到实体消息： {} ", user);
+        } catch (Exception e) {
+            log.error("监听消费消息 发生异常： ", e.fillInStackTrace());
+        }
+    }
 
     /**
      * 监听消费消息-map多类型字段信息
